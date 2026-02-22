be79990d3e4887132bf9e67ff99c5d949e753822
Nitin Gupta <gupta7nitin@gmail.com>
2026-02-22 12:05:42 +0530

    refactor: update tsup configuration to support multiple entry points and minification

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	entry: ['src/index.ts'],
-	format: ['cjs', 'esm'],
-	dts: true,
-	clean: true,
-	sourcemap: true,
-	splitting: false,
-	treeshake: true,
-});
+export default defineConfig([
+	{
+		entry: ['src/index.ts'],
+		format: ['cjs', 'esm'],
+		clean: true,
+		sourcemap: false,
+		splitting: false,
+		treeshake: true,
+		minify: true,
+	},
+	{
+		entry: ['src/index.ts'],
+		dts: { only: true },
+	},
+]);
